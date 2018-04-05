507e6d657cbece126651f220563acfc7be5a55ce
jeromeocode <jgvillap@edu.uwaterloo.ca>
2018-04-05 14:33:18 -0400

    Commented the code and updated the Readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # std-every-200-seconds
+
+This is used to calculate sample standard deviation in 200s intervals in the csv file. With this you can view how the stdev changes over time instead of calculating total deviation.
+
+## How to use it
+
+- Take this python file and place it in the same folder as the CSV file you are calcualting the stdev from.
+- Change the code to replace the file varible to the filename of your choice.
+
+```python
+#change the filename to a chosen file and make sure that file is in the same folder as this python program
+file='LT30minThermal.csv'
+```
+
+- It will then output the the time interval and the stdev of the data in that time interval
--- currentSTDevery200seconds.py
+++ currentSTDevery200seconds.py
@@ -6,13 +6,20 @@ from numpy import genfromtxt
 
 array1=[]
 
+#change the filename to a chosen file and make sure that file is in the same folder as this python program
 file='LT30minThermal.csv'
+
 with open(file) as csvfile:
-				#changed the format of csv so that they have a header
+				#changed the format of csv so that they have a header (change skip_header to 0 if there are no headers)
 				dataReader = genfromtxt(file, delimiter=',', skip_header=1)
+
 				for row in dataReader:
 					array1.append(row[1])
+
 					#row[1] contains the current data
 					if round(row[0])%200 == 0 and len(array1) > 10:
+
+						#prints the last time point in the array with the stdev beside it
 						print(row[0], statistics.stdev(array1))
+						
 						array1=[]
